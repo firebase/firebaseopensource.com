2ec52a5ceb17ebc7df5a222c2bce3aef1cad45ae
Abe Haskins <abeisgreat@abeisgreat.com>
2017-10-05 10:37:20 -0700

    Fixes tests for new module format

--- test/App.spec.ts
+++ test/App.spec.ts
@@ -3,13 +3,13 @@ import { expect } from "chai";
 import * as Vue from "vue";
 import App from "../src/App";
 
-const Constructor = Vue.extend(App);
-const vm = new Constructor().$mount();
+const vm = new App().$mount();
 
 describe("App", () => {
   it("should have a specific msg", () => {
-    expect(typeof App.data).to.equal("function");
-    expect(vm["msg"]).to.equal("Welcome to Your Vue.js App");
+    expect(vm.msg).to.equal("Welcome to Your Vue.js App");
+  });
+  it("should have some content in the HTML", () => {
     expect(vm.$el.querySelector("h2").innerHTML, "Essential Links");
   });
 });

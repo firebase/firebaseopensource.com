ab3e093526b19e2014c56d3f88ad486a67aef54b
Sam Stern <samstern@google.com>
2017-12-08 11:00:20 -0800

    Limit to 6

--- src/components/Homepage/index.ts
+++ src/components/Homepage/index.ts
@@ -63,6 +63,7 @@ export default class Projects extends Vue {
         .orderBy("stars", "desc")
         .orderBy("description")
         .where("fork", "==", false)
+        .limit(6)
         .get()
         .then(snapshot => {
           snapshot.docs.forEach(doc => {

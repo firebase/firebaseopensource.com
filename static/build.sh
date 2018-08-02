caa784efbc380e428e569e7e2a452832db13a51a
Sam <samstern@google.com>
2018-08-02 14:35:06 -0700

    UN-ignore dist

--- static/build.sh
+++ static/build.sh
@@ -4,8 +4,12 @@ set -e
 # Auth with service account
 gcloud auth activate-service-account --key-file service-account.json
 
+# Clone source
+git clone -b static-site https://github.com/firebase/firebaseopensource.com
+cd firebaseopensource.com/static
+
 # Download all HTML
-gsutil -m cp -r gs://fir-oss.appspot.com/prod/* public/
+gsutil -m cp -r gs://fir-oss.appspot.com/prod/* ./public/
 
 # Deploy with Firebase
 firebase deploy --only hosting

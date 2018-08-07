e79a3e700d8120145f2b53a5fd94cfa648792c65
Abe Haskins <abeisgreat@abeisgreat.com>
2018-08-07 15:28:01 -0700

    Adds cronjob for auto deploy

--- static/build.sh
+++ static/build.sh
@@ -2,15 +2,15 @@
 set -e
 
 # Auth with service account
-gcloud auth activate-service-account --key-file service-account.json
+gcloud auth activate-service-account --key-file /tmp/serviceaccount/service-account.json
 gcloud auth list
 
 # Clone source
-git clone -b static-site https://github.com/firebase/firebaseopensource.com
+git clone https://github.com/firebase/firebaseopensource.com
 cd firebaseopensource.com/static
 
 # Download all HTML
 gsutil -m cp -r gs://fir-oss.appspot.com/prod/* ./public/
 
 # Deploy with Firebase
-firebase --token=$FIREBASE_TOKEN deploy --only hosting
+firebase --token=$(cat /tmp/firebasetoken/token) deploy --only hosting

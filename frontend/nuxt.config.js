e98610693881577116d2bd23535041d670366038
Sam Stern <samstern@google.com>
2018-08-30 15:32:52 -0700

    Simplify nuxt config

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -14,39 +14,6 @@ module.exports = {
     fallback: true,
     routes: routes
   },
-  head: {
-    meta: [
-      {
-        charset: "utf-8"
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1"
-      }
-    ],
-    link: [
-      {
-        rel: "stylesheet",
-        href:
-          "//fonts.googleapis.com/css?family=Roboto:300,400,400italic,500,500italic,700,700italic|Roboto+Mono:400,500,700|Material+Icons"
-      },
-      {
-        rel: "icon",
-        href: "/logo-small.png"
-      },
-      {
-        rel: "stylesheet",
-        href:
-          "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css"
-      }
-    ],
-    script: [
-      {
-        src:
-          "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"
-      }
-    ]
-  },
   modules: [
     "~/modules/typescript.js",
     "@nuxtjs/sitemap"  

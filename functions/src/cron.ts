6f1ba21cea25b0abce59fa5a28cbfa5f2407a5f6
Sam Stern <samstern@google.com>
2018-11-15 16:58:37 -0800

    Fix functions

--- functions/src/cron.ts
+++ functions/src/cron.ts
@@ -1,33 +1,26 @@
-const { google } = require('googleapis');
+const { google } = require("googleapis");
 
-import * as rp from 'request-promise-native';
+import * as rp from "request-promise-native";
 
 export class Cron {
+  public static async build() {
+    const auth = await google.auth.getClient({
+      scopes: ["https://www.googleapis.com/auth/cloud-platform"]
+    });
 
-    public static async build() {
-        const auth = await google.auth.getClient({
-            scopes: ['https://www.googleapis.com/auth/cloud-platform']
-        });
+    const client = google.cloudbuild({
+      version: "v1",
+      auth: auth
+    });
 
-        const client = google.cloudbuild({
-            version: 'v1',
-            auth: auth
-        });
+    const project = await google.auth.getProjectId();
 
-        const project = await google.auth.getProjectId();
+    const params = {
+      projectId: project,
+      requestBody: require("../config/cloudbuild.json")
+    };
 
-        const params = {
-            projectId: project,
-            requestBody: require('../config/cloudbuild.json')
-        };
-
-        const res = await client.projects.builds.create(params);
-        console.log(res);
-    }
-
-    public static async fetchAllProjects() {
-        const res = await rp('https://us-central1-fir-oss.cloudfunctions.net/getAllProjects');
-        console.log(res);
-    }
-
+    const res = await client.projects.builds.create(params);
+    console.log(res);
+  }
 }

abf04a6b5b204cdf44fc0403613e6c804ca6d656
Sam Stern <samstern@google.com>
2018-11-15 17:06:09 -0800

    Fix build config

--- functions/src/cron.ts
+++ functions/src/cron.ts
@@ -1,6 +1,41 @@
 const { google } = require("googleapis");
 
-import * as rp from "request-promise-native";
+// TODO: This could be in a JSON file somewhere
+const buildConfig = {
+  secrets: [
+    {
+      kmsKeyName:
+        "projects/fir-oss/locations/global/keyRings/fosdc/cryptoKeys/FIREBASE_TOKEN",
+      secretEnv: {
+        TOKEN:
+          "CiQAltNzF5CE3iSor7QNi6sBviZUReob8uHmfWYWq1MLGOVWrtUSVgBAMMXe+C00ExhdwrOFF0AKi6ARKPxirgTwFuOXk6rBlIwmg3Rq5roI5aL7cT8jWLOQm8PWLmt+fo07QT8do08pmNWBsmpMCZDxf5J2rGxd9pO7SAma"
+      }
+    }
+  ],
+  steps: [
+    {
+      name: "gcr.io/cloud-builders/docker",
+      args: ["build", "-t", "fir-oss/static-renderer", "."]
+    },
+    {
+      name: "gcr.io/cloud-builders/docker",
+      entrypoint: "bash",
+      args: [
+        "-c",
+        "docker run -e FIREBASE_TOKEN=$$TOKEN fir-oss/static-renderer"
+      ],
+      secretEnv: ["TOKEN"]
+    }
+  ],
+  source: {
+    repoSource: {
+      projectId: "fir-oss",
+      repoName: "firebaseopensource",
+      dir: "static",
+      branchName: "master"
+    }
+  }
+};
 
 export class Cron {
   public static async build() {
@@ -17,7 +52,7 @@ export class Cron {
 
     const params = {
       projectId: project,
-      requestBody: require("../config/cloudbuild.json")
+      requestBody: buildConfig
     };
 
     const res = await client.projects.builds.create(params);
